0d703e32d38c78ef675f4ab51d59d75cd6954fd4
Rian Benedito <rian.bene14@gmail.com>
2025-05-14 21:42:59 -0300

    Configurando ESLint e Jest

---
+++ frontend/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75
+    }
+  }
+};
