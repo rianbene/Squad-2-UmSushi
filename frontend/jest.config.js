a062721a366552de1f1965d06839293fd829ffb3
Rian Benedito <rian.bene14@gmail.com>
2025-05-16 00:06:36 -0300

    Configuração ESLint e Jest - Frontend

---
+++ frontend/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75
+    }
+  }
+};
