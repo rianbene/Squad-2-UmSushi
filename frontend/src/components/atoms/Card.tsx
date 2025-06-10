9f88359347b6cf5588d8cebbc910992bb4655010
Rian Benedito <rian.bene14@gmail.com>
2025-06-09 22:08:19 -0300

    Atoms Corrigidos para tsx (#10)
    * Update and rename Button.jsx to Button.tsx
    
    mudei a linguagem para typescript
    
    * Refatoração dos Atoms
    
    * Update Button.tsx
    
    ---------
    
    Co-authored-by: caf3ina <luan13machadofranca@gmail.com>

---
+++ frontend/src/components/atoms/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface CardProps {
+  children: React.ReactNode;
+}
+
+export default function Card({ children }: CardProps) {
+  return (
+    <div className="border rounded-lg shadow p-4 bg-white">
+      {children}
+    </div>
+  );
+}
