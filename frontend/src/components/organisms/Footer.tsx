c5263eab7b2e9be53b2331b64d8fb5a879ce7417
Rian Benedito <rian.bene14@gmail.com>
2025-06-13 23:37:09 -0300

    Add organisms (#12)
    * Add organisms
    
    * Update ListaProdutos.tsx

---
+++ frontend/src/components/organisms/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 text-center text-sm p-4 mt-10">
+      <p>© 2025 UmSushi. Todos os direitos reservados.</p>
+      <div className="mt-2">
+        <a href="#" className="mx-2 text-blue-500">Instagram</a>
+        <a href="#" className="mx-2 text-blue-500">Facebook</a>
+        <a href="#" className="mx-2 text-blue-500">Contato</a>
+      </div>
+    </footer>
+  );
+}
